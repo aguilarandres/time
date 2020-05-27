69f0623c25647b4ef3ca58e16b6accbd3e625845
edo <aguilar_andres@protonmail.com>
2020-05-27 11:50:56 -0700

    changed parameter name

--- to_seconds.js
+++ to_seconds.js
@@ -1,14 +1,14 @@
 // converts integer value to seconds
-function to_seconds( milliseconds ) {
+function to_seconds(number) {
 
   // handle NaN, undefined, object
-  if(isNaN(milliseconds))
+  if(isNaN(number))
     throw new Error("Parameter is not a number")
   // handle negative values
   if(milliseconds < 0)
     throw new Error("Time value cannot be a negative value")
 
-  return milliseconds / 1000;
+  return number / 1000;
 }
 // node js exports this function as a variable stored in module.exports
 module.exports = to_seconds;
